9e2dcfe3f747cd5a581e895c05f748c1f82b81a7
Slick <holygalaxy444@gmail.com>
2020-10-19 12:48:51 +0500

    new

--- newproject/mainapp/models.py
+++ newproject/mainapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Command(models.Model):
+    command = models.CharField(max_length=128)
+    win = models.IntegerField(default=0)
+    defeat = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.command
+
+
+class Composition(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    patronymic = models.CharField(max_length=200)
+    position = models.CharField(max_length=100)
+    number = models.IntegerField(default=0)
+    command = models.CharField(max_length=128)
+
+
+    def __str__(self):
+        return self.name
+
+class Match(models.Model):
+    command = models.CharField(max_length=128)
+    rebounds = models.IntegerField(default=0)
+    result = models.IntegerField(default=0)
+    interceptions = models.IntegerField(default=0)
+    transmission = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.command
