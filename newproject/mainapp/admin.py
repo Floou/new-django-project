bd64214c77e18aded94187af4ce2abd85676ead3
Slick <holygalaxy444@gmail.com>
2020-10-22 02:33:04 +0500

    new

--- newproject/mainapp/admin.py
+++ newproject/mainapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from mainapp.models import Command, Composition, Match
 
-admin.site.register(Command)
-admin.site.register(Composition)
+from mainapp.models import Team, Trainer, LineUp, Match
+
+
+admin.site.register(Team)
+admin.site.register(Trainer)
+admin.site.register(LineUp)
 admin.site.register(Match)
--- newproject/mainapp/models.py
+++ newproject/mainapp/models.py
@@ -1,34 +1,43 @@
 from django.db import models
 
 
-class Command(models.Model):
-    command = models.CharField(max_length=128)
+class Team(models.Model):
+    name_team = models.CharField(max_length=128)
     win = models.IntegerField(default=0)
     defeat = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.command
+        return self.name_team
 
 
-class Composition(models.Model):
-    name = models.CharField(max_length=200)
-    surname = models.CharField(max_length=200)
-    patronymic = models.CharField(max_length=200)
-    position = models.CharField(max_length=100)
+class Trainer(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.surname
+
+
+class LineUp(models.Model):
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
     number = models.IntegerField(default=0)
-    command = models.CharField(max_length=128)
+    position = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
 
 
 class Match(models.Model):
-    command = models.CharField(max_length=128)
-    rebounds = models.IntegerField(default=0)
-    result = models.IntegerField(default=0)
+    command = models.ForeignKey(Team, on_delete=models.CASCADE)
+    rival = models.CharField(max_length=128)
+    result_match = models.IntegerField(default=0)
+    broadcast = models.IntegerField(default=0)
     interceptions = models.IntegerField(default=0)
-    transmission = models.IntegerField(default=0)
-    losses = models.IntegerField(default=0)
+    tool = models.IntegerField(default=0)
+    block_shot = models.IntegerField(default=0)
 
     def __str__(self):
         return self.command
