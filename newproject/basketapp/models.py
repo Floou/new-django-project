74683701ffa2289ef8d72ff2d92f702da458372a
Slick <holygalaxy444@gmail.com>
2020-11-14 17:05:57 +0500

    basketapp (new)

--- newproject/basketapp/models.py
+++ newproject/basketapp/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from mainapp.models import Trainer
+
+class TrainerBasket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    added = models.DateTimeField(auto_now_add=True)
--- newproject/basketapp/views.py
+++ newproject/basketapp/views.py
@@ -1,5 +1,22 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from basketapp.models import TrainerBasket
+from django.urls import reverse
+
+from mainapp.models import Trainer
 
 def index(request):
     return render(request, 'basketapp/basket.html')
+
+
+def add(request, trainer_id):
+    trainer = Trainer.objects.get(pk=trainer_id)
+    TrainerBasket.objects.get_or_create(
+        user=request.user,
+        trainer=trainer
+    )
+    return HttpResponseRedirect(
+        reverse('mainapp:trainer_page',
+                kwargs={'pk': trainer.pk})
+    )
